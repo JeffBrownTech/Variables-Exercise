dced5d7196f4d490d50c93863ecdbc28bf92e149
Jeff Brown <jeff@jeffbrown.tech>
2025-03-19 07:21:12 -0700

    variable exercise completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
+            string petName = "Beni";
+            int petAge = 1;
+            char petGender = 'M';
+            bool isAltered = true;
+            double petWeightKg = 2.7;
+            decimal adoptionFee = 120.00m;
+            
             Console.WriteLine("Hello, World!");
+            Console.WriteLine($"I have a pet cat named {petName}. He ({petGender}) is {petAge} year(s) old.");
+            Console.WriteLine($"His adoption fee was ${adoptionFee}.");
+            Console.WriteLine("His current status is:");
+            Console.WriteLine($"   - Neutered:      {isAltered}");
+            Console.WriteLine($"   - Weight (kg):   {petWeightKg}");
         }
     }
 }
